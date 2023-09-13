62f32adbe352aca8a0c109e2569db8c6b0ce5ce1
shiz <danyshilov5@gmail.com>
2023-09-13 19:22:46 +0300

    test version of stripe payment

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,6 +1,13 @@
+from django.shortcuts import redirect
 from rest_framework import viewsets
 from rest_framework import generics
 from rest_framework.permissions import IsAdminUser
+import stripe
+from decimal import Decimal
+from django.conf import settings
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 from .serializers import (
     GetProductSerializer, PostProductSerializer, SizeSerializer,
@@ -49,3 +56,64 @@ class CartRetrieveAPIView(generics.RetrieveAPIView):
     def update(self, request, *args, **kwargs):
         kwargs['partial'] = True
         return super().update(request, *args, **kwargs)
+    
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+class CreateCheckoutSessionAPIView(APIView):
+    def post(self, request):
+        cart = Cart.objects.get(id=request.data.get('cart_id'))
+        line_items = []
+        for item in cart.items.all():
+           line_items.append({
+              'price_data' :{
+                 'currency' : 'usd',  
+                    'product_data': {
+                        'name': item.product.title,
+                    },
+                'unit_amount': int(item.product.price*100)
+                },
+                'quantity' : item.count
+           })
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                line_items=line_items,
+                mode= 'payment',
+                success_url= 'http://localhost:8000',
+                cancel_url= 'http://localhost:8000',
+                )
+            return redirect(checkout_session.url , code=303)
+        except Exception as e:
+            print(e)
+            return e
+        
+
+class WebHook(APIView):
+  def post(self , request):
+    event = None
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+
+    try:
+      event = stripe.Webhook.construct_event(
+        payload ,sig_header , settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as err:
+        # Invalid payload
+        raise err
+    except stripe.error.SignatureVerificationError as err:
+        # Invalid signature
+        raise err
+
+    # Handle the event
+    if event.type == 'payment_intent.succeeded':
+      payment_intent = event.data.object 
+      print("--------payment_intent ---------->" , payment_intent)
+    elif event.type == 'payment_method.attached':
+      payment_method = event.data.object 
+      print("--------payment_method ---------->" , payment_method)
+    # ... handle other event types
+    else:
+      print('Unhandled event type {}'.format(event.type))
+
+    return Response({})
